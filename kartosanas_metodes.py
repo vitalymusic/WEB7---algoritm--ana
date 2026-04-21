c9561dea92ae5bb449fefe2037c8bb479854ba2a
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-04-21 19:40:29 +0300

    kattošanas metode Pytons

---
+++ kartosanas_metodes.py
@@ -0,0 +1,9 @@
+# 1. variants: 
+dati = [64, 34, 25, 12, 22, 11, 90]
+jauns_saraksts = sorted(dati) 
+
+# 2. variants: 
+dilstosa_seciba = sorted(dati, reverse=True) 
+print(f"Oriģināls: {dati}") 
+print(f"Sakārtots: {jauns_saraksts}") 
+print(f"Dilstošs: {dilstosa_seciba}")
