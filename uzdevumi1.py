bf95f39566f9aa15bc920b2f576463d117db0983
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-04-27 21:35:13 +0300

    uzdevumi

---
+++ uzdevumi1.py
@@ -0,0 +1,7 @@
+# 1. Aprēķināt taisnstūra laukumu kur malas garumu un platuumu ievada lietotājs. Izvadē uzrakstīt: "Taisnsstūra laukums ir: "
+
+garums = float(input("Ievadiet taisnstūra garumu: "))
+platums = float(input("Ievadiet taisnstūra garumu: "))
+print(f"Taisnstūra platums ir: {garums*platums:.2f}!") 
+print(f"Taisnstūra laukums ir: {(garums+ platums)*2:.2f}!") 
+
